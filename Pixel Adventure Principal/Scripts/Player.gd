145c9a190fdebbb909bd6242276b1f6b01a081ed
Misael Andrejezieski <misaelandrejezieski130982@outlook.com>
2024-01-07 14:41:33 -0300

    Update Player.gd

--- Pixel Adventure Principal/Scripts/Player.gd
+++ Pixel Adventure Principal/Scripts/Player.gd
@@ -8,12 +8,15 @@ var jump_force = -720
 
 func _physics_process(delta: float)-> void:
 	velocity.y += gravity * delta
-	
-	var move_direction = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
-	
+
+	_get_input()
+
 	if Input.is_action_pressed("jump"):
 		velocity.y	= jump_force / 4
-	velocity.x = move_speed * move_direction
 	
 	move_and_slide(velocity)
 	
+
+func _get_input():
+	var move_direction = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	velocity.x = move_speed * move_direction
