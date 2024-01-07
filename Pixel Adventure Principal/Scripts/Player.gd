9da1c14245ed3c5bf0e9c1348b2abb5e7ad2913a
Misael Andrejezieski <misaelandrejezieski130982@outlook.com>
2024-01-07 15:48:33 -0300

    Deus do céu

--- Pixel Adventure Principal/Scripts/Player.gd
+++ Pixel Adventure Principal/Scripts/Player.gd
@@ -13,9 +13,11 @@ func _physics_process(delta: float)-> void:
 	_get_input()
 	velocity = move_and_slide(velocity)
 	is_grounded =_check_is_ground()
+	_set_animation()
   
 
 func _get_input():
+	velocity.x = 0
 	var move_direction = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
 	velocity.x = lerp(velocity.x, move_speed * move_direction, 0.2)
 	
@@ -31,3 +33,14 @@ func _check_is_ground():
 		if raycast.is_colliding():
 			return true
 	return false
+
+func _set_animation():
+	var anime= "idle"
+	
+	if !is_grounded:
+		anime= "jump"
+	elif velocity.x != 0:
+		anime= "run"
+	
+	if $anime.assigned_animation != anime:
+		$anime.play(anime)
