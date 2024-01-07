d49a9334b51e98603629e6f71b928fad4622304c
Misael Andrejezieski <misaelandrejezieski130982@outlook.com>
2024-01-07 12:29:02 -0300

    Update Player.gd

--- Pixel Adventure Principal/Scripts/Player.gd
+++ Pixel Adventure Principal/Scripts/Player.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 var velocity = Vector2.ZERO
-var move_speed = 480
+var move_speed = 240
 var gravity = 1290
 var jump_force = -720
 
@@ -12,7 +12,7 @@ func _physics_process(delta: float)-> void:
 	var move_direction = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
 	
 	if Input.is_action_pressed("jump"):
-		velocity.y	= jump_force / 2
+		velocity.y	= jump_force / 4
 	velocity.x = move_speed * move_direction
 	
 	move_and_slide(velocity)
