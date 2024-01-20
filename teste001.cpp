e8f08ca1935b700266626a5e3d71166b689110c9
Misael Andrejezieski <misaelandrejezieski130982@outlook.com>
2024-01-20 20:54:28 -0300

    Update teste001.cpp

--- teste001.cpp
+++ teste001.cpp
@@ -4,65 +4,41 @@
 
 
 int main() {
-	
-	setlocale(LC_ALL,"Portuguese");
-	
-    char company_name[] = "Benco";
-    char sheet_type1[] = "chapa azul";
-    char sheet_type2[] = "chapa verde";
-    char product_names[][50] = {"morango Batavo", "tradicional Batavo", "grego morango itambé", "tradicional itambé", "light morango Batava", "light tradicional Batavo", "pessego Batavo"};
-    float sheet_per_box = 0.15;
-    float mixpaper_per_box = 0.025;
-    int boxes_per_production;
-    int total_boxes_per_day;
-    float total_sheet;
+    char nome_empresa[] = "Benco";
+    char tipo_chapa1[] = "chapa azul";
+    char tipo_chapa2[] = "chapa verde";
+    char nomes_produtos[][50] = {"morango Batavo", "tradicional Batavo", "grego morango itambé", "tradicional itambé", "light morango Batava", "light tradicional Batavo", "pessego Batavo"};
+    float chapa_por_caixa = 0.15;
+    float mixpaper_por_caixa = 0.025;
+    int caixas_por_producao;
+    int total_caixas_por_dia;
+    float total_chapa;
     float total_mixpaper;
-    char flavor[50];
-    int quantity;
-    char another_flavor;
+    int codigo_produto;
 
-    printf("Insira o sabor do iogurte: ");
-    scanf("%s", flavor);
+    printf("Insira o codigo do produto: ");
+    scanf("%d", &codigo_produto);
 
-    if (strcmp(flavor, "light") == 0) {
-        strcpy(sheet_type1, "chapa verde");
-        strcpy(sheet_type2, "chapa verde");
+    if (codigo_produto == 1 || codigo_produto == 2 || codigo_produto == 5 || codigo_produto == 6 || codigo_produto == 9) {
+        strcpy(tipo_chapa1, "chapa azul");
+        strcpy(tipo_chapa2, "chapa azul");
+    } else {
+        strcpy(tipo_chapa1, "chapa verde");
+        strcpy(tipo_chapa2, "chapa verde");
     }
 
-    printf("Insira o número de caixas por produção: ");
-    scanf("%d", &boxes_per_production);
+    printf("Insira o numero de caixas por producao: ");
+    scanf("%d", &caixas_por_producao);
 
-    total_boxes_per_day = 230;
-    total_sheet = boxes_per_production * total_boxes_per_day * sheet_per_box;
-    total_mixpaper = boxes_per_production * total_boxes_per_day * mixpaper_per_box;
+    total_caixas_por_dia = 230;
+    total_chapa = caixas_por_producao * chapa_por_caixa;
+    total_mixpaper = caixas_por_producao * mixpaper_por_caixa;
 
-    printf("Serão necessários %.2f kg de %s e %.2f kg de mixpaper para produzir %d caixas de iogurte sabor %s.\n", total_sheet, sheet_type1, total_mixpaper, boxes_per_production, flavor);
-
-    printf("Deseja inserir outro sabor? (s/n): ");
-    scanf(" %c", &another_flavor);
-
-    while (another_flavor == 's') {
-        printf("Insira o sabor do iogurte: ");
-        scanf("%s", flavor);
-
-        if (strcmp(flavor, "light") == 0) {
-            strcpy(sheet_type1, "chapa verde");
-            strcpy(sheet_type2, "chapa verde");
-        }
-
-        printf("Insira o número de caixas por produção: ");
-        scanf("%d", &boxes_per_production);
-
-        total_boxes_per_day = 230;
-        total_sheet = boxes_per_production * total_boxes_per_day * sheet_per_box;
-        total_mixpaper = boxes_per_production * total_boxes_per_day * mixpaper_per_box;
-
-        printf("Serão necessários %.2f kg de %s e %.2f kg de mixpaper para produzir %d caixas de iogurte sabor %s.\n", total_sheet, sheet_type1, total_mixpaper, boxes_per_production, flavor);
-
-        printf("Deseja inserir outro sabor? (s/n): ");
-        scanf(" %c", &another_flavor);
-    }
+    printf("\nSerao necessarios %.2f kg de %s e %.2f kg de mixpaper para produzir %d caixas de iogurte.\n", total_chapa, tipo_chapa1, total_mixpaper, caixas_por_producao);
 
     return 0;
 }
+
+
+
 
