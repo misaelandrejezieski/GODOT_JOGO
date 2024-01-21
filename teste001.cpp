62973691235043fba3726bf77f8858bddeb96628
Misael Andrejezieski <misaelandrejezieski130982@outlook.com>
2024-01-20 21:18:51 -0300

    teste

--- teste001.cpp
+++ teste001.cpp
@@ -10,6 +10,8 @@ int main() {
     char tipo_chapa2[] = "chapa verde";
     char nomes_produtos[][50] = {"morango Batavo", "tradicional Batavo", "morango itambé",
 	 "tradicional itambé", "light morango Batava", "light tradicional Batavo", "pessego Batavo"};
+    char nomes_mixpaper[][50] = {"mixpaper morango Batavo", "mixpaper tradicional Batavo", "mixpaper morango itambe",
+	 "mixpaper tradicional itambe", "mixpaper light morango Batava", "mixpaper light tradicional Batavo", "mixpaper pessego Batavo"};
     float chapa_por_caixa = 0.15;
     float mixpaper_por_caixa = 0.025;
     int caixas_por_producao;
@@ -38,13 +40,9 @@ int main() {
     total_mixpaper = caixas_por_producao * mixpaper_por_caixa;
     total_production_time = (float) caixas_por_producao / 7.6;
 
-    printf("\nSerao necessarios %.2f kg de %s", tipo_chapa1, " e %.2f kg de mixpaper para produzir %d caixas de iogurte.\n", total_chapa, tipo_chapa1, total_mixpaper, caixas_por_producao);
+    printf("\nSerao necessarios %.2f kg de %s \n %.2f kg de %s para produzir %d caixas de iogurte.\n", total_chapa, tipo_chapa1, total_mixpaper, nomes_mixpaper[codigo_produto - 1], caixas_por_producao);
     printf("O tempo necessario para produzir a demanda diaria sera de %.2f minutos.\n", total_production_time);
 
     return 0;
 }
-
-
-
-
 
