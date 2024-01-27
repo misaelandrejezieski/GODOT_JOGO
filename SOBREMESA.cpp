5a392bf2eecedc391102067a0dd5fd66436de961
Misael Andrejezieski <misaelandrejezieski130982@outlook.com>
2024-01-27 17:57:50 -0300

    teste falho

--- SOBREMESA.cpp
+++ SOBREMESA.cpp
@@ -33,8 +33,9 @@ int main() {
                 strcpy(b.tipo_chapa1, "chapa verde");
                 strcpy(b.tipo_chapa2, "chapa verde");
             } else {
-            	cout << "Código inválido! ";
-			}
+            	cout << "Codigo invalido! ";
+            	
+				}
 
             cout << "Insira o numero de caixas por producao: ";
             cin >> b.caixas_por_producao;
